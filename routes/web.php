cd3b7985ccbbf8d18347f6b31f0d35849eb8cc54
EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
2020-09-17 23:54:55 +0000

    Mm

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// LP獲得用
+Route::get('app', function () {
+    return view('fb.check'); //LP確認用
+    // return view('fb.noMatch'); //LP獲得用
+});
+
+// LP確認用
+Route::get('cccheck', function () {
+    return view('fb.noMatch');
+});
