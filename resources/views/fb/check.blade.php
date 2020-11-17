c8b4ef119e6af859c1ad787f39bb2c485f5ef3af
EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
2020-11-17 23:45:54 +0000

    mm

--- resources/views/fb/check.blade.php
+++ resources/views/fb/check.blade.php
@@ -10,9 +10,9 @@
     </head>
     <body>
         <br>
-        <img src="{{ asset('image/job_cre1.jpg') }}"></img>
+        <img src="{{ asset('image/showroom.png') }}"></img>
         <div class="button">
-                <a href="https://apps.apple.com/jp/app/バイト-アプリはタウンワーク/id434586962" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
+                <a href="https://apps.apple.com/jp/app/showroom-ライブ配信ならショールーム/id734256893" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
         </div>
         <p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>
     </body>
