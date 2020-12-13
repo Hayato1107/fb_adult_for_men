4af808cd14e5cb87c611f1fd1dea88c9f76b4d99
EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
2020-12-13 19:26:05 +0000

    Mm

--- resources/views/fb/friend.blade.php
+++ resources/views/fb/friend.blade.php
@@ -16,19 +16,22 @@
             <a href="https://lin.ee/yypCzsw" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
-        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいママ活アプリ</span>を紹介中!</p>
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいママ活</span>を紹介中!</p>
         <!--<br>-->
         <img src="{{ asset('image/m-1.jpeg') }}"></img>
         <img src="{{ asset('image/m-2.jpeg') }}"></img>
         <img src="{{ asset('image/m-3.JPG') }}"></img>
         <img src="{{ asset('image/m-4.JPG') }}"></img>
         <h1>初心者でも大丈夫! <span class="red">むしろ初心者向け！</span><br>ママ見つけやすさに定評あり!</h1>
+        <a href="https://lin.ee/yypCzsw" target="_blank" class="btn-square-little-rich"><img src="{{ asset('image/line_friend_add.png') }}"></img></a>
+        <br>
+        <br>
         <div class="button">
             <!--<a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>-->
             <a href="https://lin.ee/yypCzsw" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
-        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいママ活アプリ</span>を紹介中!</p>
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいママ活</span>を紹介中!</p>
         <br>
     </body>
 </html>
