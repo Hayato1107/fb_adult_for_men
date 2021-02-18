939ccc7e682bb105d5c7af89bd8c0ca5172ff16b
EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
2021-02-18 06:22:31 +0000

    M

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,11 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
+            return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+            // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
             // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
         }
-        return redirect('http://www.angelfc.net/mobaile_regist.php?sex=female&channel=mocom&ascode=G16531'); //モコム - Aトレード
+        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
     }
     
