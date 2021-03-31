337ce5a95a90737e1c98c01578187d5db61cc6df
EC2 Default User <ec2-user@ip-172-31-17-34.us-east-2.compute.internal>
2021-03-31 00:13:30 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,12 +10,14 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://preaf.jp/pa.do?s=v04031&o=49386&guid=ON'); //JUMP - Preaf
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=15524'); //CONNECT - fam
+            // return redirect('https://preaf.jp/pa.do?s=v04031&o=49386&guid=ON'); //JUMP - Preaf
             // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
             // return redirect('https://a-trade.jp/redirect/giftchat?media=G16531'); //ギフトチャット - Aトレード
             // return redirect('https://fam-ad.com/ad/p/r?_site=64802&_article=16786'); //ワクワク - fam
         }
-        return redirect('https://preaf.jp/pa.do?s=v04031&o=49386&guid=ON'); //JUMP - Preaf
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=15524'); //CONNECT - fam
+        // return redirect('https://preaf.jp/pa.do?s=v04031&o=49386&guid=ON'); //JUMP - Preaf
         // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
         // return redirect('https://a-trade.jp/redirect/giftchat?media=G16531'); //ギフトチャット - Aトレード
         // return redirect('https://fam-ad.com/ad/p/r?_site=64802&_article=16345'); //ワクワクweb - fam
